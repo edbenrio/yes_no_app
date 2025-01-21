e8d83c80b44333e48b990b315eb24ac9727735a6
Edgar Cano Maciel <edgarrubendario@gmail.com>
2025-01-21 18:17:40 -0300

    Tarea - Argumentos al Widget

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/shared/message_field_box.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
@@ -28,6 +31,7 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -35,11 +39,12 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                  itemCount: 10,
+                  itemCount: chatProvider.messageList.length,
                   itemBuilder: (context, index) {
-                    return index % 2 == 0
-                        ? MyMessageBubble()
-                        : HerMessageBubble();
+                    final message = chatProvider.messageList[index];
+                    return message.fromWho == FromWho.hers
+                        ? HerMessageBubble()
+                        : MyMessageBubble(message: message);
                   }),
             ),
             const MessageFieldBox(),
--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({super.key});
+  final Message message;
+  const MyMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +17,8 @@ class MyMessageBubble extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'Non nostrud.',
-              style: TextStyle(color: Colors.white),
+              message.text,
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
